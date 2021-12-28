7a310c3d53823b7eb56ae66fde85316630c1de29
OlegatorJava <Olegator1_95@mail.ru>
2021-12-28 12:51:47 +0300

    HomeWork2

--- src/main/java/Course1/Lesson2/HomeWork2.java
+++ src/main/java/Course1/Lesson2/HomeWork2.java
@@ -2,14 +2,14 @@ package Course1.Lesson2;
 
 public class HomeWork2 {
     public static void main(String[] args) {
-        System.out.println(SumofNumbers(2,4));
+        System.out.println(sumofNumbers(2,4));
         definition(15);
         definition2(-4);
-        Text ("Это Java, детка! ", 5);
+        text ("Это Java, детка! ", 5);
         leapYear(1600);
 
     }
-    public static boolean SumofNumbers(int a, int b){ //проверяет положительная или отрицательная сумма чисел
+    public static boolean sumofNumbers(int a, int b){ //проверяет положительная или отрицательная сумма чисел
         if (10 <= a + b || a + b >= 20){
             return true;
         } else {
@@ -32,7 +32,7 @@ public class HomeWork2 {
         }
     }
 
-    public static void Text (String txtToPrint, int a) { //выводит в консоль текст а раз.
+    public static void text (String txtToPrint, int a) { //выводит в консоль текст а раз.
         for (int i = 0; i < a; i++) {
             System.out.println(txtToPrint);
         }
